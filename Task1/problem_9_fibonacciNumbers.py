53084dabda8627a5ce2b657b0ecc9955dd2f02df
mustafaAtefmustafa <mustafaatef761@gmail.com>
2021-12-19 20:46:29 +0200

    Modified problem_9.

--- Task1/problem_9_fibonacciNumbers.py
+++ Task1/problem_9_fibonacciNumbers.py
@@ -1,16 +1,24 @@
 def main():
     n = int(input())
-    for i in range(0, n):
-        print(f"{get_fib(i)}", end=" ")
+
+    # Bad algorithm.
+    """for i in range(0, n):
+        print(f"{get_fib(i)}", end=" ")"""
+
+    # Better algorithm.
+    l = [0, 1]
+    for i in range(0, n - 2):
+        l.append(l[-1] + l[-2])
+    print(l)
 
 
-def get_fib(n):
+"""def get_fib(n):
     if n == 0:
         return 0
     elif n == 1:
         return 1
     else:
-        return get_fib(n - 1) + get_fib(n - 2)
+        return get_fib(n - 1) + get_fib(n - 2)"""
 
 
 if __name__ == "__main__":
