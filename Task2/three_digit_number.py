da06ba01460ab3000a673e0ef75b915a0121b676
mustafaAtefmustafa <mustafaatef761@gmail.com>
2021-12-22 03:42:52 +0200

    modified problem_3_three_digit_number

--- Task2/three_digit_number.py
+++ Task2/three_digit_number.py
@@ -2,26 +2,35 @@ import random
 
 
 def main():
-    hit_counter = 0
-    miss_counter = 0
-    n1 = list(str(random.randint(100, 999)))
+    hit = [0, 0, 0]
+    miss = [0, 0, 0]
+    n1 = list(str(223))
     print(n1)   # Just for the sake of testing,,, can be removed.
     n2 = list(input("Enter a 3-digit number: "))
     while n2 != n1:
         for i in range(0, len(n2)):
             for j in range(0, len(n1)):
                 if n2[i] == n1[j] and i == j:
-                    hit_counter += 1
+                    hit[i] = 1
+                    miss[i] = 0
                     break
                 elif n2[i] == n1[j] and i != j:
-                    miss_counter += 1
-                    break
+                    
+                    if hit[j] == 1:
+                        miss[i] = 1
+                        continue
+                    else:
+                        miss[i] = 1
+                        continue
                 else:
                     continue
-        print(f"{hit_counter} hit  {miss_counter} miss")
+        hits = sum(hit)
+        misses = sum(miss)
+        print(f"{hits} hit  {misses} miss")
         n2 = list(input("Enter a 3-digit number: "))
-        hit_counter = 0
-        miss_counter = 0
+        hit = [0, 0, 0]
+        miss = [0, 0, 0]
+    
 
 
 if __name__ == "__main__":
