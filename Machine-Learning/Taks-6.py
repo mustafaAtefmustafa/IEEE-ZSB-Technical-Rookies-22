21384b1b77acb984b45af4bfc89dd3c8094273a0
mustafaAtefmustafa <mustafaatef761@gmail.com>
2022-02-17 13:16:40 +0200

    Task6

--- Machine-Learning/Taks-6.py
+++ Machine-Learning/Taks-6.py
@@ -1,3 +1,5 @@
+"""Loading dataset"""
+
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import os
@@ -12,39 +14,45 @@ CATEGORIES = ["Dog", "Cat"]
 for category in CATEGORIES:  # do dogs and cats
     path = os.path.join(DATA_DIR, category)  # create path to dogs and cats
     for img in os.listdir(path):  # iterate over each image per dogs and cats
-        img_array = cv2.imread(os.path.join(path, img),cv2.IMREAD_GRAYSCALE)  # convert to array
-        #plt.imshow(img_array, cmap='gray')  # graph it
-        #plt.show()  # display!
+        img_array = cv2.imread(os.path.join(path, img),
+                               cv2.IMREAD_GRAYSCALE)  # convert to array
+        # plt.imshow(img_array, cmap='gray')  # graph it
+        # plt.show()  # display!
         break
     break
-    
 
 
 IMG_SIZE = 100
 new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-#plt.imshow(new_array, cmap='gray')  # graph it
-#plt.show()  # display!
+# plt.imshow(new_array, cmap='gray')  # graph it
+# plt.show()  # display!
 
 
 training_data = []
 
-def create_training_data():
-    for category in CATEGORIES:  
 
-        path = os.path.join(DATA_DIR,category)
-        class_num = CATEGORIES.index(category)  # get the classification  (0 or a 1). 0=dog 1=cat
+def create_training_data():
+    for category in CATEGORIES:
+
+        path = os.path.join(DATA_DIR, category)
+        # get the classification  (0 or a 1). 0=dog 1=cat
+        class_num = CATEGORIES.index(category)
 
         for img in tqdm(os.listdir(path)):  # iterate over each image per dogs and cats
             try:
-                img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)  # convert to array
-                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))  # resize to normalize data size
-                training_data.append([new_array, class_num])  # add this to our training_data
+                img_array = cv2.imread(os.path.join(
+                    path, img), cv2.IMREAD_GRAYSCALE)  # convert to array
+                # resize to normalize data size
+                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+                # add this to our training_data
+                training_data.append([new_array, class_num])
             except Exception as e:
                 pass
 
+
 create_training_data()
 
-#print(len(training_data))
+# print(len(training_data))
 
 random.shuffle(training_data)
 
@@ -52,7 +60,7 @@ random.shuffle(training_data)
 X = []
 y = []
 
-for features,label in training_data:
+for features, label in training_data:
     X.append(features)
     y.append(label)
 
@@ -61,10 +69,10 @@ for features,label in training_data:
 X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 
 # Save the data
-pickle_out = open("X.pickle","wb")
+pickle_out = open("X.pickle", "wb")
 pickle.dump(X, pickle_out)
 pickle_out.close()
 
-pickle_out = open("y.pickle","wb")
+pickle_out = open("y.pickle", "wb")
 pickle.dump(y, pickle_out)
 pickle_out.close()
